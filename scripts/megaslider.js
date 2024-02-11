318d6807405dd41851265e9b8e0be4060c521b64
Egor <golderandlomove@gmail.com>
2024-02-11 21:48:26 +0500

    Фикс предупреждения от Swiper. Костылем, добавляем слайд в стартовый HTML, останавливем Slider, получаем слаыйд с API, удаляем слайд-заглушку и запускаем слайдер. ХОБА. Нормального решения не нашел в нете.

--- scripts/megaslider.js
+++ scripts/megaslider.js
@@ -7,8 +7,18 @@ const slidesQuantity = 5;
 // Swiper dll делаем слайдер
 const swiper = new Swiper('.swiper', {
   // Optional parameters
+
+  // Для скипа ошибки библиотеки
+  enabled: false,
+
   direction: 'horizontal',
+
   loop: true,
+
+  keyboard: {
+    enablee: true,
+    onlyInViewport: true,
+  },
 
   autoplay: true,
 
@@ -22,33 +32,29 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
-
-console.log(swiper);
 
 const slidesAppend = function (data) {
   for (let i = 0; i < slidesQuantity - 1; i++) {
     swiper.appendSlide(`<div class="swiper-slide"><img src="${data[i].image}"></div>`);
   }
+  // Удаляем слайд-заглушку для скипа ошибки библиотеки и запускаем слайдер.
+  swiper.removeSlide(1);
+  swiper.enable();
+
+  console.log(swiper);
 };
 
 // Запрос к фейк-API на выдачу картиночек для слайдера.
-const getPic = async function () {
+(async function () {
   try {
     const response = await fetch('https://fakestoreapi.com/products/');
-    console.log(response);
     if (!response.ok) throw new Error(`Ошибка связи с FakeAPI: ${response.status}`);
     const data = await response.json();
     return data;
   } catch (error) {
     throw error;
   }
-};
-getPic()
+})()
   .then((data) => slidesAppend(data))
   .catch((error) => console.error('Ошибка работы с FakeAPI: ' + error));
